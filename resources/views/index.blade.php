23bd75d0c6595b38642791fc3b1901202594b643
AnonSeram <muabduh1@gmail.com>
2025-07-24 03:24:41 +0700

    navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -390,7 +390,7 @@
               <i class="fas fa-sign-in-alt me-2"></i>
               {{ __("messages.login") }}
             </a></li>
-            <li><a class="btn-daftar enhanced-register-btn" href="{{ route('register') }}">
+            <li><a class="btn-daftar enhanced-btn" href="{{ route('register') }}">
               <i class="fas fa-user-plus me-2"></i>
               {{ __("messages.register") }}
             </a></li>
@@ -707,6 +707,7 @@
   align-items: center;
   gap: 8px;
   transition: all 0.3s ease;
+  margin-left: 10px;
   min-width: auto;  /* atau bisa auto */
   overflow: visible; /* penting agar teks tidak terpotong */
   white-space: nowrap; /* mencegah patah baris */
