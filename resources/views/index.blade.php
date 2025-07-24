1080f947c19e7abba95264388745d9096e0d2e5c
AnonSeram <muabduh1@gmail.com>
2025-07-24 18:09:50 +0700

    daftar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -697,13 +697,24 @@
 .enhanced-register-btn {
   background: transparent;
   color: #ffffffff;
+  padding: 0px;
+  text-decoration: none;
+  font-weight: 600;
+  font-size: 0.9rem;
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  transition: all 0.3s ease;
+  margin-left: 5px;
+  overflow: visible; /* penting agar teks tidak terpotong */
+  white-space: nowrap; /* mencegah patah baris */
+  text-align: center;
+  justify-content: center;
 }
 
 .enhanced-register-btn:hover {
-  background: #99a3e3;
   color: white;
   transform: translateY(-2px);
-  box-shadow: 0 4px 15px rgba(153, 163, 227, 0.3);
 }
 
 .dropdown-divider {
