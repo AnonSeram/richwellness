9b52c196876cf518556000aa42f956c4fc4f3a18
AnonSeram <muabduh1@gmail.com>
2025-07-24 17:54:01 +0700

    daftar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -390,11 +390,11 @@
               <i class="fas fa-sign-in-alt me-2"></i>
               {{ __("messages.login") }}
             </a></li>
-            <li><a class="getstarted enhanced-register-btn" href="{{ route('register') }}">
+            <li><a class="btn-daftar enhanced-register-btn" href="{{ route('register') }}">
               <i class="fas fa-user-plus me-2"></i>
-              {{ __("messages.register") }}
-            </a></li>
+              {{ __("messages.register") }}</a></li>
           @endauth
+
         </ul>
         <i class="bi bi-list mobile-nav-toggle enhanced-mobile-toggle"></i>
       </nav>
